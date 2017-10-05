06e4efda5c87396fa7e667c017908e058c1fa254
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-10-06 02:43:02 +0300

    Created button-component.

---
+++ src/components/forms/inputs/button/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import style from './button.scss';
+
+const Button = ({ value, type, onClick, className, disabled }) => (
+  <button
+    className={cx(className, disabled ? style.buttonDis : style.button)}
+    type={!disabled ? type : 'button'}
+    onClick={e => !disabled && onClick && onClick(e)}
+  >
+    {value}
+  </button>
+);
+
+Button.propTypes = {
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  type: PropTypes.string,
+};
+
+Button.defaultProps = {
+  type: 'button',
+  className: '',
+  disabled: false,
+};
+
+export default Button;
