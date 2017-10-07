f0f4c4b18404c7e668bc209bc6dc8ff7e088dfaf
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-10-07 16:28:13 +0300

    Fixed login-route.

--- src/routes/login/components/login.js
+++ src/routes/login/components/login.js
@@ -2,14 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LoginForm from '../../../components/forms/login-form';
 
-const Login = ({ onSubmit }) => (
-  <div className="col-md-12">
+import style from './login.scss';
+
+const Login = ({ onSubmit, values, showTable }) => (
+  <div className={style.login}>
     <LoginForm onSubmit={onSubmit} />
+    <div>
+      {showTable ?
+        <table className="table table-striped" style={{ marginTop: '20px' }}>
+          <tbody>
+            <tr>
+              <td>firstName</td>
+              <td>{values.firstName}</td>
+            </tr>
+            <tr>
+              <td>lastName</td>
+              <td>{values.lastName}</td>
+            </tr>
+            {values.profession ?
+              <tr>
+                <td>profession</td>
+                <td>{values.profession.value}</td>
+              </tr>
+              : null}
+            <tr>
+              <td>phoneCode</td>
+              <td>{values.phoneCountry}</td>
+            </tr>
+            <tr>
+              <td>phoneNumber</td>
+              <td>{values.phoneNumber}</td>
+            </tr>
+          </tbody>
+        </table>
+        : null}
+    </div>
   </div>
 );
 
 Login.propTypes = {
   onSubmit: PropTypes.func.isRequired,
+  values: PropTypes.objectOf(PropTypes.any),
+  showTable: PropTypes.bool,
+};
+
+Login.defaultProps = {
+  values: {},
+  showTable: false,
 };
 
 export default Login;
--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import LoginComponent from './components/login';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { values: {}, showTable: false };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
   onSubmit(values) {
     console.log('values: ', values);
+    this.setState({ values, showTable: true });
   }
 
   render() {
     return (
-      <LoginComponent onSubmit={this.onSubmit} />
+      <LoginComponent
+        onSubmit={this.onSubmit}
+        values={this.state.values}
+        showTable={this.state.showTable}
+      />
     );
   }
 }
