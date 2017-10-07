9a37dab779087beaed1f468ae1c83b7b9d5601de
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-10-07 14:37:21 +0300

    Fixed input-component (added validation msg).

--- src/components/forms/inputs/input/index.js
+++ src/components/forms/inputs/input/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 import style from './input.scss';
 
-const Input = ({ input: { value, onChange }, meta, placeholder }) => (
-  <div className={style.wrapInput}>
+const Input = ({
+  input: { value, onChange }, meta: { touched, error }, placeholder, setRef, cssInput,
+  cssTypeGroup, cssWidth, defaultValue, readOnly, onBlur, onFocus, onChange: onChangeHandler,
+}) => (
+  <div
+    className={style.wrapInput}
+    style={cssWidth ? { width: cssWidth, marginLeft: '-2px' } : {}}
+  >
     <input
-      className={style.input}
-      onBlur={e => onChange(e.target.value)}
+      className={cx(style.input, style[cssTypeGroup], touched && error ? style.inputError : '')}
+      onBlur={e => (onChange && onChange(e.target.value)) || (onBlur && onBlur(e))}
+      onFocus={e => onFocus && onFocus(e)}
+      onChange={e => onChangeHandler && onChangeHandler(e)}
       type="text"
-      defaultValue={value}
+      defaultValue={value || defaultValue}
       placeholder={placeholder}
+      ref={setRef}
+      style={cssInput}
+      readOnly={readOnly}
     />
+    {touched && error ?
+      <div className={style.errorMsg}>
+        {error}
+      </div> : ''}
   </div>
 );
 
@@ -19,12 +35,30 @@ Input.propTypes = {
   input: PropTypes.objectOf(PropTypes.any),
   meta: PropTypes.objectOf(PropTypes.any),
   placeholder: PropTypes.string,
+  cssTypeGroup: PropTypes.oneOf(['left', 'right', 'both', 'none']),
+  cssWidth: PropTypes.string,
+  defaultValue: PropTypes.string,
+  setRef: PropTypes.func,
+  cssInput: PropTypes.objectOf(PropTypes.any),
+  readOnly: PropTypes.bool,
+  onBlur: PropTypes.func,
+  onFocus: PropTypes.func,
+  onChange: PropTypes.func,
 };
 
 Input.defaultProps = {
   input: {},
   meta: {},
   placeholder: '',
+  cssTypeGroup: 'none',
+  cssWidth: '',
+  defaultValue: '',
+  setRef: null,
+  cssInput: {},
+  readOnly: false,
+  onBlur: null,
+  onFocus: null,
+  onChange: null,
 };
 
 export default Input;
